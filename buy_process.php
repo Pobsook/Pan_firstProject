2e4fe2e5ef47002caf3f5803de641cd1bf5dc697
Pobsook <pobsook999@gmail.com>
2025-03-27 01:47:45 +0700

    fix agiain

--- buy_process.php
+++ buy_process.php
@@ -5,9 +5,9 @@ require("connect.php");
 
 if(isset($_POST['get_session_ship'])){
     if(
-        isset($_SESSION['shipping_info']['name_or_company']) &&
-        isset($_SESSION['shipping_info']['telephone']) &&
-        isset($_SESSION['shipping_info']['id_card_or_passport']) &&
+        !empty($_SESSION['shipping_info']['name_or_company']) &&
+        !empty($_SESSION['shipping_info']['telephone']) &&
+        !empty($_SESSION['shipping_info']['id_card_or_passport']) &&
         !empty($_SESSION['total_price']) &&
         $_SESSION['total_price'] != 0
     ){
